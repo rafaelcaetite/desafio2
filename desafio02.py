c782f44a02745817c9d676013e5b9078d66e121d
Rafael Martins <102500171+rafaelcaetite@users.noreply.github.com>
2022-03-28 17:36:33 -0300

    Add files via upload

---
+++ desafio02.py
@@ -0,0 +1,4 @@
+dia=input('Qual o dia do seu nascimento?')
+mês=input('Qual o mês do seu nascimento?')
+ano=input('Qual o ano do seu nascimento?')
+print('Aah, então você nasceu no dia',dia,'de',mês,'de',ano,'. Correto?')
